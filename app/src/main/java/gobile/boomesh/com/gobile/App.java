a608d8ce3df333d389c30797e05612aca7e4b6a6
Boomesh Bohn <sumesh@bnotions.com>
2015-12-24 19:15:12 -0500

    Brought in robolectric, and some libraries for testing.  Need to implement Dagger.

---
+++ app/src/main/java/gobile/boomesh/com/gobile/App.java
@@ -0,0 +1,9 @@
+package gobile.boomesh.com.gobile;
+
+import android.app.Application;
+
+/**
+ * Created by sumesh on 12/24/15.
+ */
+public class App extends Application {
+}
--- app/src/test/java/gobile/boomesh/com/gobile/ExampleUnitTest.java
+++ app/src/test/java/gobile/boomesh/com/gobile/ExampleUnitTest.java
@@ -1,15 +1,21 @@
 package gobile.boomesh.com.gobile;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 18)
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void addition_isCorrect() {
+        assertThat("asd").isNotNull();
     }
 }
