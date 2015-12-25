0d4fe3b3618197ab6867d94063081e2235c0d85b
Boomesh Bohn <sumesh@bnotions.com>
2015-12-24 20:26:40 -0500

    Added leak canary.  Going to try and DI this.

--- app/src/main/java/gobile/boomesh/com/gobile/App.java
+++ app/src/main/java/gobile/boomesh/com/gobile/App.java
@@ -2,9 +2,16 @@ package gobile.boomesh.com.gobile;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 /**
  * Custom override class of {@link Application}, for DI, potential other libraries
  * Created by sumesh on 12/24/15.
  */
 public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LeakCanary.install(this);
+    }
 }
